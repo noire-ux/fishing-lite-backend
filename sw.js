34fc6b781e6ea11bede51108b297e7cae5eeb51c
noire-ux <noire.busines@gmail.com>
2025-11-19 18:58:36 +0700

    chore: upload extracted sw.js

---
+++ sw.js
@@ -0,0 +1,37 @@
+
+self.addEventListener('push', function(event) {
+    if (event.data) {
+        const data = event.data.json();
+        const options = {
+            body: data.body,
+            icon: '/vite.svg', // Use the game icon
+            badge: '/vite.svg',
+            vibrate: [100, 50, 100],
+            data: {
+                dateOfArrival: Date.now(),
+                primaryKey: 1
+            }
+        };
+        event.waitUntil(
+            self.registration.showNotification(data.title, options)
+        );
+    }
+});
+
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    event.waitUntil(
+        clients.matchAll({type: 'window', includeUncontrolled: true}).then(function(clientList) {
+            if (clientList.length > 0) {
+                let client = clientList[0];
+                for (let i = 0; i < clientList.length; i++) {
+                    if (clientList[i].focused) {
+                        client = clientList[i];
+                    }
+                }
+                return client.focus();
+            }
+            return clients.openWindow('/');
+        })
+    );
+});
